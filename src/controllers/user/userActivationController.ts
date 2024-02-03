b8b965dfa741bd46bacf19df762e3718cfdb48e4
micc007 <mitro.michal123@gmail.com>
2024-02-03 10:53:43 +0100

    removed commented code

--- src/controllers/user/userActivationController.ts
+++ src/controllers/user/userActivationController.ts
@@ -1,11 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-// import { nanoid } from "nanoid";
-// import bcrypt from "bcrypt";
-// import { randomUUID } from "crypto";
 import { findActToken, activateAccount } from '../../config/mysql';
-// import { mail } from "../../config/mailer";
 import { actTokenType } from "../../ts/types/user/actTokenType";
-// import { emailDataType } from "../../ts/types/email/emailDataType";
 
 import dotenv from 'dotenv';
 dotenv.config();
