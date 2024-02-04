fdb9d060aa9a67f4fe83c92ae94fe328e94fae1e
micc007 <mitro.michal123@gmail.com>
2024-02-04 20:29:11 +0100

    ttl check implemented, changed messages to slovakian

--- src/controllers/user/setPassController.ts
+++ src/controllers/user/setPassController.ts
@@ -13,28 +13,32 @@ dotenv.config();
 
 const setPassController = async (req: Request, res: Response, next: NextFunction) => {
     
-    if(!req.params.token) return res.status(400).send("Token not provided");
+    if(!req.params.token) return res.status(400).send("Token neexistuje");
     const b = req.body;
-    if(b.pass !== b.pass2) return res.status(200).send("Passwords are not identical");
+    if(b.pass !== b.pass2) return res.status(200).send("Heslá sa nezhodujú");
 
     findPassToken([req.params.token])
         .then(async (data) => {
-            if(data.length === 0) return res.status(400).send("Password recovery link has expired");
+            if(data.length === 0) return res.status(400).send("Platnosť odkazu na obnovu hesla vypršala");
             const result: findPassType[] = data as findPassType[];
 
-            const setPassData: setPassType = {
-                pass: await bcrypt.hash(b.pass, 10),
-                user_id: result[0].user_id
+            const ttl: number = new Date(result[0].pass_rst_ttl).getTime();
+            if(ttl > Date.now()) {
+                const setPassData: setPassType = {
+                    pass: await bcrypt.hash(b.pass, 10),
+                    user_id: result[0].user_id
+                }
+    
+                setPass(setPassData)
+                    .then(() => {
+                        return res.status(200).send("Nové heslo uložené");
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        return res.status(500).send("Error");
+                    });
             }
-
-            setPass(setPassData)
-                .then(() => {
-                    return res.status(200).send("New password was set");
-                })
-                .catch(err => {
-                    console.log(err);
-                    return res.status(500).send("Error");
-                })
+            else return res.status(200).send("Platnosť odkazu vypršala, skúste znova obnoviť heslo");
 
         })
         .catch(err => {
